c54e663298b1f5e2b169a029cd3eb4df9302442b
Steven J Bark <stevenjbark@gmail.com>
2020-03-26 21:59:18 -0500

    SJB Commit: Added CorrelationMaps Folder, Finalized more on the Choropleth Maps

--- ChoroplethJSCode/Texas_Counties/static/js/logic.js
+++ ChoroplethJSCode/Texas_Counties/static/js/logic.js
@@ -1,7 +1,6 @@
 //Adapting code from Project 2, no URL needed for geojson files. Use D3 to select dropdown menu 
 //information and open proper file.
 
-
 // Creating map object
   var myMap = L.map('map', {
     center: [30.3, -99.0],
@@ -23,6 +22,97 @@
 
 
 
+//Create initial function
+function init() {
+
+ //Input the Year information into opening the proper geojson file.
+ var geoData = `static/data/2020_TXCounty_Health_Data.geojson`;
+
+ var geojson;
+
+ //Read in data with d3
+ d3.json(geoData, function(data) {
+
+   //Test that data extraction is working for statesData.geojson file.
+   //console.log(data);
+   // console.log(data.features);
+   // console.log(data.features[0].properties.Years_Potential_Life_Lost_Per_100K);
+
+   // //As Justin's example, create new chloropleth layer
+   geojson = L.choropleth(data, {
+
+     
+     //Justin's code includes a valueProperty that take data from the geojson "properties" dictionary.
+     //This will allow us to use whatever properties data we want for mapping.
+     valueProperty: "Years_Potential_Life_Lost_Per_100K",
+
+     //Set color scale
+     scale: ["blue", "green", "yellow", "red"],
+
+     //Number of breaks in step range
+     steps: 15,
+
+     // q for quartile, e for equidistant, k for k-means
+     mode: 'e',
+     style: {
+       // Border color
+       color: "black",
+       weight: 1,
+       fillOpacity: 0.5,
+     },
+     
+
+     //Binding a pop-up to each layer
+     onEachFeature: function (features, layer) {
+       layer.bindPopup(
+         features.properties.County + " " + features.properties.Year +
+         `<br>Years_Potential_Life_Lost_Per_100K:<br>` +
+         features.properties.Years_Potential_Life_Lost_Per_100K
+       )
+       //console.log(features.properties.Years_Potential_Life_Lost_Per_100K)
+     }
+
+   }).addTo(myMap);
+
+
+
+ // Set up the legend
+   var legend = L.control({ position: 'topright' });
+   legend.onAdd = function () {
+     var div = L.DomUtil.create('div', 'info legend');
+     var limits = geojson.options.limits;
+     var colors = geojson.options.colors;
+     var labels = [];
+
+     // Add min & max
+     var legendInfo =
+       `<h4>Years_Potential_Life_Lost_Per_100K</h4>` +
+       '<div class="labels">' +
+       '<div class="min">' +
+       limits[0] +
+       '</div>' +
+       '<div class="max">' +
+       limits[limits.length - 1] +
+       '</div>' +
+       '</div>';
+
+     div.innerHTML = legendInfo;
+
+     limits.forEach(function (limit, index) {
+       labels.push('<li style="background-color: ' + colors[index] + '"></li>');
+     });
+
+     div.innerHTML += '<ul>' + labels.join('') + '</ul>';
+     return div;
+   };
+
+   // Adding legend to the map
+   legend.addTo(myMap);
+
+
+ })
+
+};
 
 
 
@@ -31,17 +121,23 @@
 //d3.select("#button").on("click", location.reload());
 
 //Event handler for dropdown menu change and update of map from Year Input.
+//d3.select("#selYear").on("change", updateMap);
+//d3.select("#selYear").on("change", location.reload());
 d3.select("#selYear").on("change", updateMap);
 
+
 //Event handler for dropdown menu change and update of map from Health Metric Input.
+//d3.select("#selHealthMetric").on("change", location.reload());
 d3.select("#selHealthMetric").on("change", updateMap)
+
 
 
 
 //Create function to updateMap
 function updateMap() {
 
-
+  d3.selectAll("g > *").remove();
+  d3.select("#leaflet-right > *").remove();
 
   //SELECTION DATA FROM DROPDOWN MENUS
   //Select dropdown menu and assign to variable dropdownYear.
@@ -52,6 +148,7 @@ function updateMap() {
   var Year = dropdownYear.property("value");
   console.log(Year);
 
+  
 
   //Health Data from Dropdown Menu
   //Select health data dropdown menu and assign to variable dropdownMetric.
@@ -107,7 +204,7 @@ function updateMap() {
         layer.bindPopup(
           features.properties.County + " " + features.properties.Year +
           `<br>${Metric}:<br>` +
-          Metric
+          features.properties.Metric
         )
         //console.log(features.properties.Years_Potential_Life_Lost_Per_100K)
       }
@@ -154,4 +251,4 @@ function updateMap() {
 
 };
 
-updateMap()
+init()

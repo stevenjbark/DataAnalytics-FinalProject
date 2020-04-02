48dc07ae47e1e0f9086bf6866f30576d853a6854
Raquel McLellan <raquelmclellan@gmail.com>
2020-04-02 13:13:05 -0500

    flask move

--- Flask/app.py
+++ Flask/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 #MONGODB CONNECTION AND ACCESS OF DATABASE COLLECTIONS
 #Setup MongoDB connection as in scrape_mars.py. Conn = connection port info, 
-#client is connection client.
+#client is connection client. Note: Using flask_pymongo, so this code is different than pymongo.
 app.config["MONGO_URI"] = "mongodb://localhost:27017/ProjectHealth_DB"
 mongo = PyMongo(app)
 
@@ -15,12 +15,6 @@ mongo = PyMongo(app)
 StateData = mongo.db.StateData
 CountyData = mongo.db.CountyData
 
-
-#ROUTES FOR INDEX AND DATA URLS
-#Route to render the index.html page with data from the ACAData_DB database
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
 
 #SETUP API'S FOR ACCESSING DATA IN MongoDB. 
 #The find({}, {"_id":0}) basically amends find() to select everything (first {} set), 
@@ -36,7 +30,6 @@ def State():
 def County():
     county_data = list(CountyData.find({}, {"_id": 0}))
     return jsonify(county_data)
-
 
 
 #LOAD OF RANDOM FOREST REGRESSOR MODEL (RF_Regressor.pkl)
@@ -67,17 +60,40 @@ def process_input(data):
     
     return df
 
-#SETUP MAIN INDEX PAGE TO INCLUDE REGRESSION MODEL
-@app.route("/", methods=["GET", "POST"])
+#ROUTES FOR INDEX AND DATA URLS
+#Route to render the index.html page with data from the ACAData_DB database
+@app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/About")
+def about():
+    return render_template("about.html")
+
+@app.route("/Explore")
+def explore():
+    return render_template("explore.html")
+
+
+@app.route("/tableau")
+def tableau():
+    return render_template("tableau.html")
+
+#SETUP MAIN INDEX PAGE TO INCLUDE REGRESSION MODEL
+#This can be another route with index.html above ("/" and render_template("index.html") and 
+#this route being "/other_route_name" and render_template("otherpage.html")
+@app.route("/Models", methods=["GET", "POST"])
+def models():
     if request.method == "POST":
         load_model()
         input_data = request.form.to_dict()
         newdata = process_input(input_data)
         value = RFmodel.predict(newdata)
-        return render_template("index.html", result=value)
+        return render_template("models.html", result=value)
 
-    return render_template("index.html")
+    return render_template("models.html")
+
+
 
 
 if __name__ == "__main__":
